d3ed07ef8b67f76fd7110ba295567027bd7ed6c7
Hookyard <strippolijules@gmail.com>
2022-03-31 15:16:29 +0200

    copie de secour

--- src/Controller/MovieListController.php
+++ src/Controller/MovieListController.php
@@ -2,8 +2,11 @@
 
 namespace App\Controller;
 
+use App\Dto\RechercheDto;
+use App\Form\RechercheType;
 use MovieListDto;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -11,17 +14,63 @@ class MovieListController extends AbstractController
 {
     #[Route('/movie/list/{id}', name: 'app_movie_list', methods: ["GET", "POST"])]
 
-    public function index($id): Response
+    public function index($id, Request $request): Response
     {
         $movieApiDto = new MovieListDto();
-        $moviesPage = $movieApiDto->getPopular($id);
 
-        //DD($moviesPage);
+        $recherche = new RechercheDto();
+        $form = $this->createForm(RechercheType::class);
+
+        $form->handleRequest($request);
+        $data = $form->getData();
+        if(!$data == null){
+            $movies = $movieApiDto->getFilmsByName($data->getInput());
+        }else{
+            $movies = $movieApiDto->getPopular($id);
+        }
+
+        //Remplacer par page
+        $categories = $movieApiDto->getCategories();
+
+        //DD($movies);
         return $this->render('movie_list/index.html.twig', [
             'controller_name' => 'MovieListController',
-            'movies' => $moviesPage,
+            'movies' => $movies ,
             'id' => $id + 1,
-            'pagesPrecedente' => $id - 1
+            'pagesPrecedente' => $id - 1,
+            'controller' => $movieApiDto,
+            'categories'=>$categories,
+            'form'=>$form->createView(),
         ]);
     }
+
+    #[Route('/films', name: 'filmsByName')]
+    public function filmbyname(): Response
+    {
+        $movieAppDto = new MovieApiDto();
+        //Remplacer par page
+        $categorie = $movieAppDto->getCategories();
+        return $this->render('films/index.html.twig', [
+            'controller_name' => 'FilmsController',
+            'categorie' => $categorie,
+            'controller' => $movieAppDto,
+        ]);
+    }
+
+    #[Route('/testpage/{idCategory}', name: 'test')]
+    public function tester($idCategory): Response
+    {
+        $recherche = new RechercheDto();
+        $form = $this->createForm(RechercheType::class,$recherche);
+
+        $movieAppDto = new MovieListDto();
+        $filmCategories = $movieAppDto->getFilmByCategoryId($idCategory,20);
+        return $this->render('accueil/index.html.twig', [
+            'controller_name' => 'Accueil Controller',
+            'form' => $form,
+            'movies' => $filmCategories,
+        ]);
+    }
+
+
 }
